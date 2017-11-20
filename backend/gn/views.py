8bc3c91ed503d96b641b8c9d2876f2b2ffa0d216
jpham197 <jpham197@gmail.com>
2017-11-20 16:15:54 -0500

    Got load route to work for backend and changed a few filepaths in frontend

--- backend/gn/views.py
+++ backend/gn/views.py
@@ -11,20 +11,31 @@ mp3s = {"mp3s":[{"filepath": "../../soundfiles/1.mp3"},
                 {"filepath": "../../soundfiles/3.mp3"},
                 {"filepath": "../../soundfiles/4.mp3"}]}
 
+accounts = {"Accounts:":[{"admin": "password"},
+    	  				 {"user1": "password"},
+    		 			 {"user2": "password"},
+    		 			 {"user3": "password"}]}
+
 @app.route("/test") #returns number of filepaths entered in json object
 def wha_t():
     a = 0
-    for num in mp3s['filepaths']:
+    for num in mp3s['mp3s']:
         a += 1
-    return str(a)
+    return "Number of filepaths in json object" + str(a)
 @app.route("/database")
 def make_db():
     con = sql.connect("main.db")
     with con:
         cur = con.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS database(id INTEGER PRIMARY KEY, filepath TEXT, )')
+        cur.execute('CREATE TABLE IF NOT EXISTS database(id INTEGER PRIMARY KEY, filepath TEXT, username TEXT)')
         # cur.execute('DROP TABLE IF EXISTS database')
         return "Success!"
+@app.route("/dropdb")
+def drop_db():
+	con = sql.connect("main.db")
+	with con:
+		cur = con.cursor()
+		cur.execute('DROP TABLE IF EXISTS database')
 @app.route("/addinfo")
 def add_data():
     a = request.args.get('a', 0, int)
@@ -44,5 +55,19 @@ def add_data_by_json():
             jsonSize += 1
             cur.execute('SELECT max(id) from database')
             entries = cur.fetchall()
-            cur.execute('INSERT INTO database (filepath) VALUES (?)', (mp3s['filepath']))
-    return str(entries)
+            cur.execute('INSERT INTO database (filepath) VALUES (?)', [num['filepath']])
+    return "entries: " + str(jsonSize)
+@app.route("/load")
+def load_db():
+	con = sql.connect("main.db")
+	con.row_factory = sql.Row
+	with con:
+		cur = con.cursor()
+		cur.execute("""
+			SELECT * FROM database
+			ORDER BY id""")
+		rows = cur.fetchall()
+		results = []
+	for row in rows:
+		results.append(tuple(row))
+	return json.dumps(results)
