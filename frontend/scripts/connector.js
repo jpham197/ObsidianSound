3ad095ab7e1fbb7c2586de894e8eeb947b49e2cd
jpham197 <jpham197@gmail.com>
2017-11-22 13:02:46 -0500

    added getData function to pull from database

--- frontend/scripts/connector.js
+++ frontend/scripts/connector.js
@@ -1,4 +1,15 @@
-export function getFilepaths() {
-	var filepaths = fetch('http://127.0.0.1:5000/load')
-	return filepaths;
+const url = "http://127.0.0.1:5000/load";
+let p = document.getElementById('1');
+
+function getData() {
+	fetch(url)
+	.then((resp) => resp.json()) // Transform the data into json
+	.then(function(data) {
+		for (a = 1; a < 11; a++) {
+			document.getElementById('t' + a).innerHTML = data[a-1];
+		}
+	})
+	.catch(function(error){
+		console.log(error);
+	});
 }
